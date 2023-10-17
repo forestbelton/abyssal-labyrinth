77b3640e8be2047795f241b292ed2261518100fe
Forest Belton <forest@homolo.gy>
2023-10-16 22:17:33 -0700

    Set base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/abyssal-labyrinth/",
   plugins: [topLevelAwait()],
 });
